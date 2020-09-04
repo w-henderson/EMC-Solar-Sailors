92a6807cdd7fa82dae2687dbd832f8997ed2d841
William Henderson <william-henderson@outlook.com>
2020-09-04 17:15:27 +0100

    Finally made first simulation!

---
+++ coordinateSystems.py
@@ -0,0 +1,39 @@
+import math
+
+class Vector:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.magnitude = math.sqrt(x**2 + y**2)
+        self.normalized = self if self.magnitude == 1 or self.magnitude == 0 else Vector(x/self.magnitude, y/self.magnitude)
+
+    def toTuple(self):
+        return (self.x,self.y)
+
+    def toPoint(self,size=10):
+        return (self.x - size, self.y - size, self.x + size, self.y + size) # (x1,y1,x2,y2)
+
+    def __add__(self,b):
+        return Vector(self.x+b.x, self.y+b.y)
+    def __sub__(self,b):
+        return Vector(self.x-b.x, self.y-b.y)
+    def __mul__(self,b):
+        return Vector(self.x*b, self.y*b)
+    def __div__(self,b):
+        return Vector(self.x/b, self.y/b)
+
+class Sun:
+    position = Vector(960,540) # Position of sun in screen space
+
+class Heliocentric:
+    def __init__(self,long,distance,scale):
+        self.long = long
+        self.distance = distance
+        self.scale = scale
+
+    def toVector(self):
+        sin = math.sin(math.radians(self.long)) # Sine of hypotenuse
+        cos = math.cos(math.radians(self.long)) # Cosine of hypotenuse
+        distance = self.distance # Distance from the sun, measured in Astronomical Units, equal to about 150 million km
+        positionRelativeToSun = Vector(distance * sin, distance * cos) # Position of planet relative to sun
+        return Sun.position + positionRelativeToSun * self.scale # Position of planet in screen space
--- planetaryPositions.py
+++ planetaryPositions.py
@@ -1,10 +1,19 @@
-import solarsystem
-import datetime
-import argparse
-import math
-from PIL import Image
+##### EMC SOLAR SAILORS #####
+# Programming by William Henderson and Elliot Whybrow
+# Physics and maths by Frankie Lambert, Ella Ireland-Carson and Ollie Temple
+# https://www.exetermathematicsschool.ac.uk/exeter-mathematics-certificate/
 
-from vector import Vector
+import solarsystem # To get planet position data for different dates
+import datetime # To parse and manipulate dates
+import argparse # To parse arguments
+import shutil # To recursively remove output directory
+import math # To do trigonometry
+import os # To make output directory
+
+from PIL import Image, ImageDraw, ImageFont # To render images
+from coordinateSystems import Vector, Heliocentric, Sun # Custom classes for coordinate systems
+
+simulationScale = 250
 
 parser = argparse.ArgumentParser(description="Solar Sailors EMC Project")
 parser.add_argument("date", type=str, help="The date to launch.") # format dd/mm/yyyy
@@ -12,25 +21,51 @@ parser.add_argument("mass", type=float, help="Mass of the spacecraft.")
 args = parser.parse_args()
 
 dateParts = args.date.split("/")
-date = datetime.datetime(int(dateParts[2]),int(dateParts[1]),int(dateParts[0]),12)
+launchDate = datetime.datetime(int(dateParts[2]),int(dateParts[1]),int(dateParts[0]),12)
 
+# Parse solar system object with given datetime into dictionary of Heliocentric coordinate system objects
 def datetimeToPositions(dt):
-    return solarsystem.heliocentric.Heliocentric(year=dt.year, month=dt.month, day=dt.day, hour=dt.hour, minute=dt.minute)
+    solarSystemObject = solarsystem.heliocentric.Heliocentric(year=dt.year, month=dt.month, day=dt.day, hour=dt.hour, minute=dt.minute)
+    solarSystemObject = solarSystemObject.planets()
+    solarSystemHeliocentric = {}
+    for planet in solarSystemObject.keys():
+        planetObject = solarSystemObject[planet] # tuple of (longitude, latitude, distance)
+        solarSystemHeliocentric[planet] = Heliocentric(planetObject[0], planetObject[2], simulationScale)
+    return solarSystemHeliocentric
 
-def heliocentricToXY(heliocentric,scale):
-    sin = math.sin(math.radians(heliocentric[0]))
-    cos = math.cos(math.radians(heliocentric[1]))
-    distance = heliocentric[1] # Measured in Astronomical Units, equal to about 150 million km
-    sunPosition = Vector(960,540)
-    positionRelativeToSun = Vector(distance * sin, distance * cos)
-    screenPosition = sunPosition + positionRelativeToSun * scale
-
+# Render frame of the simulation
 def render():
     image = Image.new("RGB", Vector(1920,1080).toTuple())
+    font = ImageFont.truetype("font_inter.otf",20)
+    draw = ImageDraw.Draw(image)
+
+    draw.ellipse(Sun.position.toPoint(size=25), fill="yellow") # Render sun
+    draw.text((0,0), "Date: "+currentSimulatedDate.strftime("%d/%m/%y"), fill="red", font=font) # Render info text
+    
     for planet in planets.keys():
-        planets[planet]
+        draw.ellipse(planets[planet].toVector().toPoint(), fill="white")
+        draw.text(planets[planet].toVector().toTuple(), planet, fill="red", font=font)
+    image.save("simulationOutput/frame"+str(currentFrame).zfill(4)+".png")
 
-planetPositionsObject = datetimeToPositions(date)
-planets = planetPositionsObject.planets()
+planets = {}
+currentSimulatedDate = 0
+currentFrame = 0
 
-print(planets)
+def simulate(startDate,cutoff=365): # Launch date is a datetime object and cutoff is the number of days to simulate
+    global planets
+    global currentSimulatedDate
+    global currentFrame
+
+    if "simulationOutput" in os.listdir(): shutil.rmtree("simulationOutput")
+    os.mkdir("simulationOutput")
+
+    for date in (startDate + datetime.timedelta(n) for n in range(cutoff)):
+        planets = datetimeToPositions(date)
+        currentSimulatedDate = date
+        currentFrame += 1
+        render()
+        print("Rendered simulation for "+date.strftime("%d/%m/%y")+"...", end="\r")
+
+    os.system('ffmpeg -i "simulationOutput/frame%04d.png" -r 30 simulation.avi')
+
+simulate(launchDate)
