8e355187a90fa4ba4650df606d5b742c04fbb149
William Henderson <william-henderson@outlook.com>
2020-09-04 18:57:08 +0100

    Improved render quality

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 # EMC Solar Sailors
 EMC Solar Sailors is a project from Exeter Mathematics School in which we are designing a spacecraft to harness the power of photons to get to Mars. This repo contains the code for our simulations and calculations.
 
-### Credits
-- William Henderson
-- Elliot Whybrow
+# Credits
+
+## Programming
+- [William Henderson](https://github.com/w-henderson)
+- [Elliot Whybrow](https://github.com/flauntingspade4)
+
+## Physics and Maths
 - Frankie Lambert
 - Ella Ireland-Carson
 - Ollie Temple
--- planetaryPositions.py
+++ planetaryPositions.py
@@ -34,38 +34,31 @@ def datetimeToPositions(dt):
     return solarSystemHeliocentric
 
 # Render frame of the simulation
-def render():
+def render(planets,date,frame):
     image = Image.new("RGB", Vector(1920,1080).toTuple())
     font = ImageFont.truetype("font_inter.otf",20)
     draw = ImageDraw.Draw(image)
 
     draw.ellipse(Sun.position.toPoint(size=25), fill="yellow") # Render sun
-    draw.text((0,0), "Date: "+currentSimulatedDate.strftime("%d/%m/%y"), fill="red", font=font) # Render info text
+    draw.text((0,0), "Date: "+date.strftime("%d/%m/%y"), fill="red", font=font) # Render info text
     
     for planet in planets.keys():
         draw.ellipse(planets[planet].toVector().toPoint(), fill="white")
         draw.text(planets[planet].toVector().toTuple(), planet, fill="red", font=font)
-    image.save("simulationOutput/frame"+str(currentFrame).zfill(4)+".png")
-
-planets = {}
-currentSimulatedDate = 0
-currentFrame = 0
+    image.save("simulationOutput/frame"+str(frame).zfill(4)+".png")
 
 def simulate(startDate,cutoff=365): # Launch date is a datetime object and cutoff is the number of days to simulate
-    global planets
-    global currentSimulatedDate
-    global currentFrame
-
     if "simulationOutput" in os.listdir(): shutil.rmtree("simulationOutput")
     os.mkdir("simulationOutput")
+    currentFrame = 0
 
     for date in (startDate + datetime.timedelta(n) for n in range(cutoff)):
         planets = datetimeToPositions(date)
         currentSimulatedDate = date
         currentFrame += 1
-        render()
+        render(planets,currentSimulatedDate,currentFrame)
         print("Rendered simulation for "+date.strftime("%d/%m/%y")+"...", end="\r")
 
-    os.system('ffmpeg -i "simulationOutput/frame%04d.png" -r 30 simulation.avi')
+    os.system('ffmpeg -i "simulationOutput/frame%04d.png" -r 30 -c:v libx264 -crf 22 simulation.mp4')
 
 simulate(launchDate)
