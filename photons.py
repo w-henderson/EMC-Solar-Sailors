cbcbe4a26e1a7d5e94b447c730a1e069e16d3172
William Henderson <william-henderson@outlook.com>
2020-09-07 11:53:18 +0100

    Converted photons.py to a class

--- photons.py
+++ photons.py
@@ -3,17 +3,11 @@
 # Physics and maths by Frankie Lambert, Ella Ireland-Carson and Ollie Temple
 # https://www.exetermathematicsschool.ac.uk/exeter-mathematics-certificate/
 
-import argparse
-h = float(6.62607004e-34) #Plank's Constant
-c = float(299792458) #Speed of light
-PowerSun = float(3.846e26) #Power output of the sun in watts
+from constants import Constants
 
-parser = argparse.ArgumentParser(description="Energy of a single photons for a given wavelength and how many photons are emmitted by the sun per second")
-parser.add_argument("wavelength", type=float, help="wavelength of light in nm")
-args = parser.parse_args()
-
-wavelength = args.wavelength * 1e-9
-E1=float(h*c/wavelength)
-print("Energy of one photon = " + str(E1) + " J")
-PhotonsPerSec = float(PowerSun/E1)
-print("Photons emmited by the sun per second = "+ str(PhotonsPerSec))
+class Photon:
+    def __init__(self,wavelength):
+        self.wavelength = wavelength
+        wavelengthInM = wavelength * 1e-9
+        self.energy = (Constants.h * Constants.c) / wavelengthInM
+        self.perSecond = Constants.sunPower / self.energy
